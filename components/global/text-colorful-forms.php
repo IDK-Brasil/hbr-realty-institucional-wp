7f7cc4ad46d4be69926c35409c15963bdd42e155
Anna Luiza de Camargo Fistarol <luizafistarol@gmail.com>
2026-01-07 11:26:40 -0300

    feat: enhance text colorful forms component with variant support and styling adjustments

--- components/global/text-colorful-forms.php
+++ components/global/text-colorful-forms.php
@@ -2,6 +2,7 @@
 $text       = is_string($text ?? null) ? $text : null;
 $color      = is_string($color ?? null) ? $color : '#FFDF80';
 $section_id = is_string($idSection ?? null) ? $idSection : '';
+$variant    = is_string($variant ?? null) ? $variant : '';
 ?>
 
 <section class="text-colorful-forms-idk-1225" id="<?php echo esc_attr($section_id); ?>">
@@ -10,7 +11,7 @@ $section_id = is_string($idSection ?? null) ? $idSection : '';
             <div class="text-colorful-forms-bg-left" style="border-color: <?php echo esc_attr($color); ?>;"></div>
             <div class="text-colorful-forms-bg-right" style="border-color: <?php echo esc_attr($color); ?>;"></div>
 
-            <div class="text-colorful-forms-text-wrapper">
+            <div class="text-colorful-forms-text-wrapper <?php echo esc_attr($variant); ?>">
                 <h2><?php echo esc_html($text); ?></h2>
             </div>
         </div>
